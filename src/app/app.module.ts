b33d3cc54db35620852baaa3578f71a7750386ef
Linus Castelino <castelinolinus@gmail.com>
2018-05-04 11:29:35 +0530

    updated UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-import {MasonryModule} from 'angular2-masonry';    //for responsive behaviour of image cards
+import {MasonryModule} from 'angular2-masonry';    //for responsive behaviour of image cards (Masonry is a JavaScript grid layout library. It works by placing elements in optimal position based on available vertical space, sort of like a mason fitting stones in a wall.)
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
